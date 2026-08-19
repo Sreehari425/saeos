93f804d77841c6fc45f29013bd68b7471324222f
Sreehari Anil <sreehari7102008@gmail.com>
2026-08-19 20:10:04 +0530

    dev: init the project

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A Nix Flake for a bare-metal Rust Operating System";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+            cargo-bootimage
+            qemu
+            llvm
+          ];
+
+          shellHook = ''
+            echo "🦀 Rust OS Development Environment Loaded!"
+            echo "Target toolchain: $(rustc --version)"
+          '';
+        };
+      }
+    );
+}
